29187f3a968afba9bcdb98060dcc3081ce0b6c15
Travis C. LaGrone <22419287+travis-c-lagrone@users.noreply.github.com>
2019-10-03 23:19:35 -0500

    pipenv-install --dev isort

---
+++ src/multinomial_naive_bayes.py
@@ -0,0 +1,8 @@
+import numpy as np
+from sklearn.base import BaseEstimator
+
+class MultinomialNaiveBayes(
+    BaseEstimator):  # noqa: D101
+
+    def __init__(self):  # noqa: D107
+        super()
