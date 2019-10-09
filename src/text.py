44edd8d92424e0158aeb0758656625b5f97cd8f2
Travis C. LaGrone <22419287+travis-c-lagrone@users.noreply.github.com>
2019-10-09 18:55:22 -0500

    Implement the text.Bernoulli class

--- src/text.py
+++ src/text.py
@@ -2,7 +2,7 @@ from collections import Counter
 from typing import Collection, Dict, List, Set
 
 from nltk import word_tokenize
-from numpy import array, ndarray
+from numpy import array, clip, ndarray
 
 
 class BagOfWords:
@@ -15,7 +15,7 @@ class BagOfWords:
     """
 
     def transform(self, docs: Collection[str]) -> ndarray:
-        """Encode a collection of textual documents with the bag-of-words model.
+        """Featurize a collection of textual documents with the bag-of-words model.
 
         Args:
             docs (Collection[str]): Zero-or-more nonnull text documents.
@@ -27,7 +27,7 @@ class BagOfWords:
                 The ordering of the vocabulary is exactly the ordering of the corresponding columns in the returned dataset.
                 The exact nature of the tokenization algorithm is an opaque implementation detail.
 
-            ndarray: The 2d integral array representing the bag-of-words of the documents.
+            ndarray: The 2d integral array representing the bag-of-words featurization of the documents.
                 Each row is a document.
                 Each column is a word.
                 Each entry is the absolute frequency the corresponding word in the corresponding document.
@@ -50,6 +50,33 @@ class BagOfWords:
 
 
 class Bernoulli:  # noqa: D101
+    """A textual data preprocessor that implements the bernoulli model.
+
+    This class complies with the Scikit-Learn `Transformer protocol`_.
+
+    .. _`Transformer protocol`: https://scikit-learn.org/stable/developers/contributing.html#apis-of-scikit-learn-objects
+
+    """
 
     def transform(self, docs: Collection[str]) -> ndarray:
-        pass  # TODO Bernoulli.transform(self, docs)
+        """Featurize a collection of textual documents with the bernoulli model.
+
+        Args:
+            docs (Collection[str]): Zero-or-more nonnull text documents.
+                Each element is the complete textual _content_ of a document.
+
+        Returns:
+            List[str]: The lexicographically-ordered vocabulary extracted from the collection of documents.
+                The cardinality of the vocabulary is exactly the width of the returned dataset.
+                The ordering of the vocabulary is exactly the ordering of the corresponding columns in the returned dataset.
+                The exact nature of the tokenization algorithm is an opaque implementation detail.
+
+            ndarray: The 2d integral array representing the bernoulli featurization of the documents.
+                Each row is a document.
+                Each column is a word.
+                Each entry is the absolute frequency the corresponding word in the corresponding document.
+
+        """
+        words, bags = BagOfWords().transform(docs)
+        bags = bags.clip(max=1)
+        return words, bags
